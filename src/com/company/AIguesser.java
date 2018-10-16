e50191edda68e4f6a08040b848898b83c472ed09
jacksonnerdcamp <jacksonjjacobs@gmail.com>
2018-10-15 22:00:05 -0400

    Finished Lab!

--- src/com/company/AIguesser.java
+++ src/com/company/AIguesser.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class AIguesser
 {
-    int n = 50;
+    /*int n = 50;
     Scanner input = new Scanner(System.in);
-    System.out.("Is this your number:" + n);
+    System.out.("Is this your number:" + n);*/
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,47 +7,56 @@ public class Main
 
     public static void main(String[] args)
 	{
-		double randomDouble = Math.random();
-		int randomInt = (int)(randomDouble * 10);
+		//Here I am asking the user for their name.
 		Scanner input = new Scanner(System.in);
-		System.out.println("guess");
-		int guess = input.nextInt();
-		while(guess != randomInt)
-		{
-			System.out.println("Guess again");
-			guess = input.nextInt();
-		}
-		System.out.println("you got it!");
-
-		//AI guesser
-		int min = 0;
-		int max = 100;
-		int guesser = (max + min)/2;
+		System.out.println("What is your name?");
+		String name = input.nextLine();
+		//Here I am asking the user which game they want to play.
 		input = new Scanner(System.in);
-		System.out.println("Is this your number:" + guesser);
-		String answer = input.nextLine();
-		if(answer.equals("yes"))
+		System.out.println("Hello " + name + ", would you like to activate Guessing Game or AI?");
+		String typedAnswer = input.nextLine();
+		if(typedAnswer.equals("Guessing Game"))
 		{
-			System.out.println("I did it!");
-		}
-		else
-		{
+			double randomDouble = Math.random();
+			int randomInt = (int) (randomDouble * 10);
 			input = new Scanner(System.in);
-			System.out.println("Higher or lower?");
-			String answer = input.nextLine();
-			if(answer.equals("higher"))
-			{
-				min += max/2;
-				guesser = (max + min)/2;
-				System.out.println("Is this your number:" + guesser);
+			System.out.println("Type in your guess, " + name);
+			int guess = input.nextInt();
+			while (guess != randomInt) {
+				System.out.println("Guess again");
+				guess = input.nextInt();
 			}
-			else
-			{
-				max -= max/2;
-				guesser = (max + min)/2;
-				System.out.println("Is this your number:" + guesser);
+			System.out.println("Congratulations, " + name + ", you got it!");
+		}
+		else if(typedAnswer.equals("AI"))
+		{
+			//AI guesser
+			int min = 0;
+			int max = 100;
+			int guesser = (max + min) / 2;
+			input = new Scanner(System.in);
+			System.out.println(name + ", is this your number:" + guesser);
+			while (true) {
+				String answer = input.nextLine();
+
+				if (answer.equals("yes")) {
+					System.out.println("I did it!");
+					break;
+				} else {
+					input = new Scanner(System.in);
+					System.out.println("higher or lower?");
+					String answer2 = input.nextLine();
+					if (answer2.equals("higher")) {
+						min = guesser;
+						guesser = (max + min) / 2;
+						System.out.println(name + ", is this your number:" + guesser);
+					} else {
+						max = guesser;
+						guesser = (max + min) / 2;
+						System.out.println(name + ", is this your number:" + guesser);
+					}
+				}
 			}
 		}
-		
     }
 }
